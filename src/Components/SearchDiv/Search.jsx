8c012444e3b0639ae01f80db9ab4cba5b02f2c92
khaihoan <khaihoan.ai@gmail.com>
2024-12-28 22:32:01 +0700

    khaihoan update 28/12

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -23,6 +23,7 @@ const Search = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const workersPerPage = 8;
+  const [profileImageUrl, setProfileImageUrl] = useState('default-avatar.png');
 
   const navigate = useNavigate();
   const handleViewProfile = (workerId) => {
@@ -178,13 +179,15 @@ const Search = () => {
   );
 
   const WorkerCard = ({ worker }) => {
-    const { user, experienceYears, bio, rating, verified } = worker;
-
+    const {profileImage, user, experienceYears, bio, rating, verified } = worker;
+    const imageUrl = profileImage 
+            ? `${API_ENDPOINT}${profileImage}` 
+            : '/default-avatar.png'; 
     return (
       <div className="bg-white rounded-xl p-6 transition-all duration-300 hover:shadow-xl">
         <div className="flex items-start gap-4">
           <img
-            src="src\Assets\images\thodien.jpg"
+            src={imageUrl}
             alt={`${user.fullName}'s profile`}
             className="w-20 h-20 rounded-xl object-cover"
           />
